eb3652d90e72289e0c30ec7ce154876d966488ff
Hay88Git <myvercel2024@gmail.com>
2024-06-18 10:07:57 +0700

    update commit

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ const openMenu=function(){
     let menu=document.querySelector("ul");
     menu.style.right="-100%";
   }
+
+function url(){
+  location.href = "https://www.hay88.one/?inviteCode=9676963";
+}
