036935ab2524dec7648460502edb8b8fd8ade25d
Hay88Git <myvercel2024@gmail.com>
2024-06-18 09:08:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const openMenu=function(){
+    let menu=document.querySelector("ul");
+    menu.style.right="0%";
+  }
+  const closeMenu=function(){
+    let menu=document.querySelector("ul");
+    menu.style.right="-100%";
+  }
